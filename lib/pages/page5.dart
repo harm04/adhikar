ef3c66b7490d2592fbd5be471a9aebdd1f5f7aae
harm04 <har04mali@gmail.com>
2024-09-27 22:06:16 +0530

    create bottom bar

---
+++ lib/pages/page5.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class page5 extends StatelessWidget {
+  const page5({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('page5'));
+  }
+}
